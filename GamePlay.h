3e609d1a98529e92713b7e18aaf529415168139b
sbhattarai5 <45582703+sbhattarai5@users.noreply.github.com>
2019-05-30 08:43:27 -0500

    Add files via upload

---
+++ GamePlay.h
@@ -0,0 +1,22 @@
+//Name: Saurav Bhattarai
+//File: GamePlay.h
+
+#ifndef GAMEPLAY_H
+#define GAMEPLAY_H
+#include "Fleet.h"
+#include <list>
+#include "SpaceShip.h"
+#include "Explosions.h"
+#include "Laser.h"
+#include "Includes.h"
+
+void check_collison(Fleet *, std::list< Laser > &, SpaceShip &, std::list< Explosion > &, bool &, Sound &);
+void draw_health(Fleet *, Surface & surface);
+void print_new_level(int level, Surface & surface);
+void print_score(const SpaceShip &, Surface &);
+void draw_explosion(std::list< Explosion > & explosions, Surface & surface);
+void draw_lives(int lives, Image * img, Surface & surface);
+#endif
+
+
+
